1a41aac3d0c989ec1c1d9dd3f78572bc5dbcf34c
Shivam63941 <shivam63941@gmail.com>
2025-07-26 01:10:09 +0530

    Update RoomCard layout and styles

--- client/src/components/RoomCard.jsx
+++ client/src/components/RoomCard.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./RoomCard.css";
 import { Link } from "react-router-dom";
-const RoomCard=({room})=>{
-    return(
-        <div className="room-card">
-            <img src={room.image} alt={room.name}/>
-            <h3>{room.name}</h3>
-            <p>{room.description}</p>
-            <p className="price">{room.price}</p>
-          <Link to="/booking">Book Now</Link>
-        </div>
-    );
+
+const RoomCard = ({ room }) => {
+  return (
+    <div className="room-card">
+      <img src={room.image} alt={room.name} />
+      <h3>{room.name}</h3>
+      <p>{room.description}</p>
+      <p className="price">{room.price}</p>
+      <Link to="/booking" className="book-btn">Book Now</Link>
+    </div>
+  );
 };
 
 export default RoomCard;
